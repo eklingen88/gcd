74eba8f4b47c7d4cbee0829f73c453f50951c261
Eric M. Klingensmith <eklingensmith@checkerdist.com>
2017-03-10 13:41:09 -0500

    Made application self-updating if local git repo is found

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ cd "${current_dir}"
 # Load config
 source includes/load-config.sh
 
+# If we have a git repo here, let's make sure THIS script is up-to-date too while we're at it
+if [ -d .git ]; then
+    git pull origin master
+fi
+
 # Pull everything down
 cd "${source_dir}"
 echo [`date`]                                       >> "${log_path}" 2>&1
